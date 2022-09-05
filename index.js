7b4df53d0b7a967f4b40f381ff724e81487e3a4e
branden1155 <brandenp119@gmail.com>
2022-09-04 21:42:10 -0400

    Updated para Details - Finished

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const express = require('express');
 const app = express();
 
 //GET /Products/all HTTP/1.1
-app.get('/products/all/:pageNumber-:sortInfo-:orderInfo', (request, response) => {
-    response.send(`GET Products:  ${request.params.pageNumber}, ${request.params.sortInfo}, ${request.params.orderInfo}`)
+app.get('/products/all/:page-:sort-:order', (request, response) => {
+    response.send(`GET Products:  ${request.params.page}, ${request.params.sort}, ${request.params.order}`)
 });
 
 //GET /Products/8719 HTTP/1.1
-app.get('/products/:numberId', (request, response) => {
-    response.send(`GET Products: ${request.params.numberId}`)
+app.get('/products/:Id', (request, response) => {
+    response.send(`GET Products: ${request.params.Id}`)
 });
 
 //GET /Products/8719-small-red HTTP/1.1
-app.get('/products/:productId-:productSize-:productColor', (request, response) => {
-    response.send(`GET Products: ${request.params.productId}, ${request.params.productSize}, ${request.params.productColor}`)
+app.get('/products/:numberId-:size-:color', (request, response) => {
+    response.send(`GET Products: ${request.params.numberId}, ${request.params.size}, ${request.params.color}`)
 });
 
 app.listen(3000);
